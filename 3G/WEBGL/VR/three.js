8c9887dc034ce2368547fa4bc7b3ef6d1295293a
hiro-9999 <47706312+hiro-9999@users.noreply.github.com>
2019-10-26 09:28:17 +0900

    Update three.js

--- 3G/WEBGL/VR/three.js
+++ 3G/WEBGL/VR/three.js
@@ -1,4 +1,20 @@
-//
+ThreeJS 入門
+https://qiita.com/tushuhei/items/2509e7e23172ec402110
+
+    ThreeJS
+        言わずと知れた 3D グラフィクス描写ライブラリ。歴史も古く、豊富なドキュメントと実装例が強み。WebVR に特化しているわけではありません。
+    Aframe
+        Mozilla が発表した WebVR 構築ライブラリ。まるで HTML を書くように、要素を追加していくようにして WebVR 体験を記述することができます。
+    WebVR starter kit
+        Three.JS ベースの WebVR 構築ライブラリ。
+    VR View
+        Google が提供する WebVR 構築ライブラリ。360 画像や動画に特化している（という理解）。
+    GUI で操作できるもの
+        http://vizor.io/
+        http://spaces.archilogic.com/3d/template/new?mode=edit&view-menu=none
+
+
+//no
 https://qiita.com/kingpanda/items/7969c5248f0d21a99910
 
 //no
